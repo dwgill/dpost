668f95140e2c31b877804fcf3150a59e74865cad
dwgill <daniel.gill@my.wheaton.edu>
2013-12-13 18:45:55 -0600

    Fix everything

--- dpost/pos.py
+++ dpost/pos.py
@@ -5,20 +5,21 @@ Created on Aug 22, 2013
 '''
 
 
+from __future__ import division
 import nltk
 from nltk.corpus import PlaintextCorpusReader
 import nltk.tokenize
 from nltk import FreqDist
 from collections import defaultdict
 import sys
+import operator
+from itertools import izip
 
 res = './res/'
 training_corpus = 'wonderland.txt'
 testing_corpus  = 'looking-glass.txt'
 # training_corpus = '*.txt'
 
-# LaPlace Smoothing
-K = 1
 
 # 1. Load a (training) corpus.
 # In the code below, the corpus will be
@@ -30,18 +31,18 @@ all_text_lower = (x.lower() for x in all_text)
 freq_dist = FreqDist(all_text_lower)
 
 # make a reduced vocabulary (here, 500 types)
-vocab = freq_dist.keys() # [:500]
+vocab = freq_dist.keys()[:500]
 vocab = vocab + ["***"]
 
 # 2. Make a reduced form of the PennTB tagset
 
-tag_formating = {'N':'noun-like','V':'verb-like','AJ':'adjective-like',
+tg_formating = {'N':'noun-like','V':'verb-like','AJ':'adjective-like',
         'AV':'adverb-like','I':'interjection','S':'symbol','G':'grouping',
         'E':'end-of-sentence', 'UN':'unknown'}
 
 penntb_to_reduced = {}
 # noun-like
-for x in ['NN', 'NNS', 'NNP', 'NNPS', 'PRP', 'EX', 'WP'] :
+for x in ['NN', 'NNS', 'NNP', 'NNPS', 'PRP', 'EX', 'WP', 'FW', 'UH'] :
     penntb_to_reduced[x] = 'N'
 # verb-like
 for x in ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ', 'MD', 'TO'] :
@@ -53,20 +54,14 @@ for x in ['POS', 'PRP$', 'WP$', 'JJ', 'JJR', 'JJS', 'DT',
 # adverb-like
 for x in ['RB', 'RBR', 'RBS', 'WRB', 'RP', 'IN', 'CC']:
     penntb_to_reduced[x] = 'AV'
-# interjections
-for x in ['FW', 'UH'] :
-    penntb_to_reduced[x] = 'I'
-# symbols
-for x in ['SYM', '$', '#'] :
-    penntb_to_reduced[x] = 'S'
 # groupings
-for x in ['\'\'', '(', ')', ',', ':', '``'] :
+for x in ['\'\'', '(', ')', ',', ':', '``', 'SYM', '$', '#'] :
     penntb_to_reduced[x] = 'G'
 # end-of-sentence symbols
 penntb_to_reduced['.'] = 'E'
 # unidentified phenomena
 
-reduced_tags = ['N', 'V', 'AJ', 'AV', 'I', 'S', 'G', 'E']
+reduced_tgs = ['N', 'V', 'AJ', 'AV', 'G', 'E']
 
 # 3. tag the corpus
 all_tagged = nltk.pos_tag(all_text)
@@ -74,94 +69,107 @@ all_tagged = nltk.pos_tag(all_text)
 # 4. make the probability matrices
 
 # a tally from types to tags; a tally from tags to next tags
-# For now, these are objective.
-tag_word_tally = {tag:{word:0 for word in vocab} for tag in reduced_tags}
-tag_transition_tally = {prev_tag:{current_tag:0
-    for current_tag in reduced_tags} for prev_tag in reduced_tags}
+tg_word_tally = {tg:{word:1 for word in vocab} for tg in reduced_tgs}
 
-previous_tag = 'E' # "ending" will be the dummy initial tag
-for word, current_tag in all_tagged:
-    if current_tag == '-NONE-':
-        continue
+tg_trans_tally = {prev_tg:{current_tg:1 for current_tg
+    in reduced_tgs} for prev_tg in reduced_tgs}
 
+tg_totals = {tg:1 for tg in reduced_tgs}
+
+previous_tg = 'E' # "ending" will be the dummy initial tag
+for word, current_tg in all_tagged:
     word = word.lower()
-    current_tag = penntb_to_reduced[current_tag]
-    if word not in tag_word_tally:
-        word = '***'
-    tag_word_tally[current_tag][word] += 1
-    tag_transition_tally[previous_tag][current_tag] += 1
-    previous_tag = current_tag
-tag_transition_tally[previous_tag]['E'] += 1
+
+    if current_tg == '-NONE-':
+        if word[0] == '.' or word[0] == '!' or word[0] == '?':
+            current_tg = 'E'
+        else:
+            current_tg = 'G'
+    else:
+        current_tg = penntb_to_reduced[current_tg]
+
+    if word in vocab:
+        tg_word_tally[current_tg][word] += 1
+
+    tg_totals[current_tg] += 1
+    tg_trans_tally[previous_tg][current_tg] += 1
+
+    previous_tg = current_tg
+
+
+# LaPlace Smoothing
+K = 1
 
 # now, make the actual transition probability matrices 
-trans_probs = {}
-for i_tag in reduced_tags:
-    i_tag_count = sum(tag_transition_tally[i_tag].values())
-    # LaPlace smoothing---add the fabricated samples to the total.
-    i_tag_count_s = i_tag_count + len(reduced_tags) * K
-    for t_tag in reduced_tags:
-        it_count = tag_transition_tally[i_tag][t_tag]
-        # LaPlace smoothing---add a fabricated sample
-        it_count_s = it_count + K
-        trans_probs[(i_tag, t_tag)] = it_count_s / float(i_tag_count_s)
 
-emit_probs = {}
-for tag, counts_dict in tag_word_tally.iteritems():
-    t_count_s = sum(counts_dict.values()) + K * len(counts_dict)
-    for word, tw_count in counts_dict.iteritems():
-        tw_count_s = tw_count + K
-        emit_probs[(tag,word)] = tw_count_s / float(t_count_s)
+trans_probs = {i_tg: defaultdict(lambda: K / (tg_totals[i_tg] + K *
+    len(tg_word_tally[i_tg].values()))) for i_tg in reduced_tgs}
 
-def pos_tagging(sequence, trans_probs, emit_probs, vocab, tags, start) :
-    viterbi = {tag:[0.0 for item in sequence] for tag in tags}
-    backpointer = {tag: [start for item in sequence] for tag in tags}
+for i_tg in reduced_tgs:
+    for f_tg in reduced_tgs:
+        trans_probs[i_tg][f_tg] = tg_trans_tally[i_tg][f_tg] / tg_totals[i_tg]
 
 
-    # Initialization
-    for tag in tags:
-        viterbi[tag][0] = trans_probs[(start, tag)] * emit_probs[(tag, sequence[0])]
-        backpointer[tag][0] = start
 
-    def determine_backpointer(tag, index):
-        key = lambda prev_tag: viterbi[prev_tag][index - 1] * trans_probs[(prev_tag, tag)]
-        max_prev_tag = max(tags, key=key)
-        backpointer[tag][index] = max_prev_tag
+emit_probs = {i_tg: defaultdict(lambda: K / (tg_totals[i_tg] +
+    K * len(tg_word_tally[i_tg].values()))) for i_tg in reduced_tgs}
 
-    def determine_prob_given_hist(tag, index):
-        word = sequence[index] if sequence[index] in vocab else '***'
-        key = lambda prev_tag: (viterbi[prev_tag][index - 1] * trans_probs[(prev_tag, tag)]
-                * emit_probs[(tag, word)])
-        max_prob = max(key(prev_tag) for prev_tag in tags)
-        viterbi[tag][index] = max_prob
+for tg in reduced_tgs:
+    for wd in vocab:
+        emit_probs[tg][wd] = tg_word_tally[tg][wd] / tg_totals[tg]
+
+
+def pos_tagging(sequence, trans_probs, emit_probs, vocab, tags, start):
+    viterbi = [{}]
+    best_tg_sqs = {}
+
+    for tg in tags:
+        viterbi[0][tg] = trans_probs[start][tg] * emit_probs[tg][sequence[0]]
+        best_tg_sqs[tg] = [tg]
+
+    def eval_prev(curr_tg, prev_tg, index):
+        indie_prob = viterbi[index - 1][prev_tg]
+        trans_prob = trans_probs[prev_tg][curr_tg]
+        emit_prob  = emit_probs[curr_tg][sequence[index]]
+        return (indie_prob * trans_prob * emit_prob), prev_tg
 
     for index in range(1, len(sequence)):
-        for tag in tags:
-            determine_backpointer(tag, index)
-            determine_prob_given_hist(tag, index)
+        viterbi.append({})
+        new_best_tg_sqs = {}
+        for tg in tags:
+            candidates = (eval_prev(tg, prev_tg, index)
+                    for prev_tg in tags)
+            prob_given_prev, prev_tg = max(candidates,
+                    key=operator.itemgetter(0))
+            viterbi[index][tg] = prob_given_prev
+            new_best_tg_sqs[tg] = best_tg_sqs[prev_tg] + [tg]
+        best_tg_sqs = new_best_tg_sqs
 
-    last_index = len(sequence) - 1
-    last_tag = max(tags, key = lambda tag: viterbi[tag][last_index])
+    final_candidates = ((viterbi[index][tg], tg) for tg in tags)
+    _, best_tg = max(final_candidates, key=operator.itemgetter(0))
 
-    tagged = []
-    for index in range(last_index, -1, -1):
-        next_word = "{word}\t\t{tag} ({qtag})".format(word=sequence[index],
-                tag=last_tag, qtag=tag_formating[last_tag])
-        tagged.append(next_word)
-        last_tag = backpointer[last_tag][index]
-    tagged.append('dwgill tagger:')
-    tagged.reverse()
-    tagged.append('\nnltk tagger:')
-
-    for word,tag in nltk.pos_tag(sequence):
-        tag = penntb_to_reduced[tag] if tag in penntb_to_reduced else '??'
-        tagged.append("{word}\t\t{tag} ({qtag})".format(word=word,tag=tag,
-            qtag=tag_formating[tag]))
-    return '\n'.join(tagged)
+    return best_tg_sqs[best_tg]
 
 # 6. try it out: run the algorithm on the test data
 tokenizer = nltk.tokenize.TreebankWordTokenizer()
 
 def tag_string(string):
     sequence = tokenizer.tokenize(string.strip().lower())
-    print pos_tagging(sequence, trans_probs, emit_probs, vocab,
-            reduced_tags, 'E')
+    my_result = pos_tagging(sequence, trans_probs, emit_probs, vocab,
+            reduced_tgs, 'E')
+
+    nltk_result = (penntb_to_reduced[x[1]] for x in nltk.pos_tag(sequence))
+    line_frmt = '{word}:\t{tag} ({tag_big})'
+    my_result_l = ['dwgill results:']
+    nltk_result_l = ['nltk results:']
+
+    my_result_l.extend(line_frmt.format(word=word,tag=tag,
+        tag_big=tg_formating[tag])
+            for (word, tag) in izip(sequence,my_result))
+    nltk_result_l.extend(line_frmt.format(word=word,tag=tag,
+        tag_big=tg_formating[tag])
+            for (word, tag) in izip(sequence,nltk_result))
+
+    print '\n\t'.join(my_result_l)
+    print '\n'
+    print '\n\t'.join(nltk_result_l)
